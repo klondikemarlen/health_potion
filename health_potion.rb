ca199ae5523a7e694319f19702084b64ec9cb3e0
Marlen Brunner <klondikemarlen@gmail.com>
2019-08-30 17:27:54 -0700

    Enable Rack base via a Response object.
    WHY: the response object converts from normal Flask style (handling minimal arguments) into Rack style.

--- health_potion.rb
+++ health_potion.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+require 'rack'
 require_relative 'config/initializer'
+require_relative 'response'
 
 class HealthPotion
   attr_reader :routes, :name, :routes_by_name, :env
@@ -18,6 +20,18 @@ class HealthPotion
 
   def run
     puts 'Booting up Health Potion ...'
+    Rack::Handler::WEBrick.run self, Port: 5000
   end
+
+  # A Rack application is a Ruby object (not a class) that responds to call.
+  # It takes exactly one argument, the environment and returns an Array of
+  # exactly three values: The status, the headers, and the body.
+  def call(env)
+    @env = env
+    Response.new(routes[path]).call
+  end
+
+  def path
+    env['REQUEST_PATH']
   end
 end
---
+++ response.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class Response
+  attr_reader :data
+
+  def initialize(handler)
+    @data = handler.call
+    @data = build_valid_response_from_data
+  end
+
+  def call
+    data
+  end
+
+  def default_headers
+    { 'Content-Type' => 'text/html' }
+  end
+
+  def default_status
+    '200'
+  end
+
+  #######
+  private
+  #######
+
+  def build_valid_response_from_data
+    return data if valid_response?
+
+    return [default_status, default_headers, [data]] if only_a_body?
+
+    return [data[1].to_s, default_headers, [data]] if body_and_status?
+
+    [data[1], data[2], [data[0]]]
+  end
+
+  def body_and_status?
+    eachable? && valid_status? && data.length == 2
+  end
+
+  def eachable?
+    data.respond_to?(:each)
+  end
+
+  def only_a_body?
+    data.is_a? String
+  end
+
+  def valid_response?
+    eachable? && valid_status? && data.length == 3
+  end
+
+  def valid_status?
+    status = data[0]
+    100 <= status && status <= 599
+  end
+end
